0c705ae28caf15d1fe345002ee8430f3c93ac2a3
vokey <lexypig@gmail.com>
2015-03-16 01:29:59 +0800

    update

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-$(document).ready(function() {
+$(document).ready(
+	function() {
 	$('.contact img').hover(function() {
 		$(this).fadeTo(100,0.5);
 	}, function() {
 		$(this).fadeTo(200,1);
 	});
+	function scrollNav() {
+  $('.nav a').click(function(){  
+	    //Toggle Class
+	    $(".active").removeClass("active");      
+	    $(this).closest('li').addClass("active");
+	    var theClass = $(this).attr("class");
+	    $('.'+theClass).parent('li').addClass('active');
+	    //Animate
+	    $('html, body').stop().animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top - 30
+	    }, 400);
+	    return false;
+	  });
+	  $('.scrollTop a').scrollTop();
+	}
+	scrollNav();
 });
+
